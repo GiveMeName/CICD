c8b7c4a84f57065c0d20b6d13d18fd657138956b
CANVAS <peter833112001@gmail.com>
2023-01-19 22:57:58 +0800

    feat: init

---
+++ src/tests/example.test.ts
@@ -0,0 +1,11 @@
+import { addNum } from "../testFunc";
+
+test('addNum1', () => {
+    const result = addNum(1, 2);
+    expect(result).toBe(3);
+});
+
+test('addNum2', () => {
+    const result = addNum(1, 5);
+    expect(result).toBe(7);
+});
